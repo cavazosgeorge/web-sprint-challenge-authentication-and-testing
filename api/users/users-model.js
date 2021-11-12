ad63364ae0f9bf19ab6b8f3b318631c665f973e4
George <cavazosgeorge@yahoo.com>
2021-11-12 17:18:06 -0500

    Users-Model: findBy function created

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -3,3 +3,7 @@ const db = require("../../data/dbConfig");
 function find() {
   return db("users");
 }
+
+function findBy(filter) {
+  return db("users").select("id", "username", "password").where(filter);
+}
