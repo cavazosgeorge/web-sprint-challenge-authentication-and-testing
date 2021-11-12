727ae7ee0f3f755bc1fca8d7c66d799fd2d1e161
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-12 18:59:38 -0500

    Fixed username taken on login bug

--- api/middleware/auth-middleware.js
+++ api/middleware/auth-middleware.js
@@ -1,43 +1,16 @@
-const dbConfig = require("../../data/dbConfig");
-const Users = require("../users/users-model");
+const db = require("../../data/dbConfig");
+const bcrypt = require("bcryptjs");
 
-const checkBody = (req, res, next) => {
-  const { username, password } = req.body;
-
-  if (
-    !username ||
-    username.trim() === "" ||
-    typeof username !== "string" ||
-    !password
-  ) {
-    return next({
-      status: 400,
-      message: "username and password required",
-    });
-  }
-  next();
-};
-
-const checkUsername = async (req, res, next) => {
-  try {
-    const { username } = req.body;
-    const existingUsername = await Users.findBy({ username }).first();
-
-    if (existingUsername) {
-      next({
-        status: 422,
-        message: "username taken",
-      });
-    } else {
-      next();
-    }
-  } catch (error) {
-    next(error);
+const checkPayload = async (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    next({ status: 401, message: "username and password required" });
+  } else {
+    next();
   }
 };
 
 const usernameExists = async (req, res, next) => {
-  const exists = await dbConfig("users")
+  const exists = await db("users")
     .select("username")
     .where("username", req.body.username)
     .first();
@@ -48,8 +21,36 @@ const usernameExists = async (req, res, next) => {
   }
 };
 
-module.exports = {
-  checkBody,
-  checkUsername,
-  usernameExists,
+const checkUsername = async (req, res, next) => {
+  try {
+    const exists = await db("users")
+      .select("username", "password")
+      .where("username", req.body.username)
+      .first();
+
+    if (!exists) {
+      next({ status: 401, message: "invalid credentials" });
+    } else if (bcrypt.compareSync(req.body.password, exists.password)) {
+      req.username = exists;
+      next();
+    } else {
+      next({ status: 401, message: "invalid credentials" });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
+
+module.exports = { usernameExists, checkUsername, checkPayload };
+
+// if (exists) {
+//     if (bcrypt.compareSync(req.body.password, exists.password)) {
+//       req.username = exists;
+//       next();
+//     } else {
+//       next({ status: 401, message: "invalid credentials" });
+
+//     }
+//   } else {
+//   next({ status: 401, message: "invalid credentials" });
+//   }
