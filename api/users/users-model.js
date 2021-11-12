a0d7ec68e11d940819739e4cf0dd82eda32d1c52
George <cavazosgeorge@yahoo.com>
2021-11-12 17:18:45 -0500

    Users-Model: findById function created

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -7,3 +7,10 @@ function find() {
 function findBy(filter) {
   return db("users").select("id", "username", "password").where(filter);
 }
+
+function findById(id) {
+  return db("users")
+    .select("id", "username", "password")
+    .where("id", id)
+    .first();
+}
