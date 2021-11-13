85a4a046870d16301fe6cb4e7c60350371e62d73
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-12 19:11:16 -0500

    Test file created: Register test passing!

--- api/server.test.js
+++ api/server.test.js
@@ -2,7 +2,22 @@ const request = require("supertest");
 const server = require("../api/server");
 const db = require("../data/dbConfig");
 const jokes = require("./jokes/jokes-data");
+
 // Write your tests here
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(true).toBe(true);
+});
+
+describe("Register", () => {
+  test("Creates a new user, and returns 201 status", async () => {
+    const res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "jaxhacks", password: "1234" });
+    expect(res.status).toBe(201);
+  });
 });
