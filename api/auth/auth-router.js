00f9fc6143ab64de542796fd513a6904c2c62e8f
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-12 19:00:00 -0500

    Login finally working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs");
+const db = require("../../data/dbConfig");
+const { JWT_SECRET } = require("../secrets/index");
 const jwt = require("jsonwebtoken");
-const { checkBody, usernameExists } = require("../middleware/auth-middleware");
-const { JWT_SECRET } = require("../secrets");
-const Users = require("../users/users-model");
+const bcrypt = require("bcryptjs");
+const {
+  checkUsername,
+  checkPayload,
+  usernameExists,
+} = require("../middleware/auth-middleware");
 
-// BUILD FUNCTION TOKEN
+// BUILD TOKEN FUNCTION
 function buildToken(user) {
   const payload = {
     id: user.id,
@@ -18,68 +22,42 @@ function buildToken(user) {
   return token;
 }
 
-router.post("/register", checkBody, usernameExists, (req, res, next) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 6);
-  user.password = hash;
+// ADD MODEL
+async function add(user) {
+  const result = await db("users").insert(user);
+  const id = result[0];
+  return findById(id);
+}
+// FIND BY ID MODEL
+async function findById(id) {
+  const result = await db("users")
+    .select("username", "id", "password")
+    .where("id", id);
+  return result;
+}
 
-  Users.add(user)
-    .then((newUser) => {
-      res.status(201).json(newUser);
+router.post("/register", checkPayload, usernameExists, (req, res, next) => {
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+  add({ username, password: hash })
+    .then((user) => {
+      res.status(201).json(user[0]);
     })
-    .catch(next);
+    .catch((error) => {
+      next(error);
+    });
 });
-/*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
-
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
-
-router.post("/login", (req, res) => {
-  res.end("implement login, please!");
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+router.post("/login", checkPayload, checkUsername, (req, res, next) => {
+  try {
+    const token = buildToken(req.username);
+    res.status(200).json({
+      message: `${req.username.username} is back!`,
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
