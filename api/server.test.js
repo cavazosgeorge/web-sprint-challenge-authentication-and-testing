54f47d566c9f480442389ee15043a5f33069d551
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-12 19:04:38 -0500

    File strcuture created

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,8 @@
+const request = require("supertest");
+const server = require("../api/server");
+const db = require("../data/dbConfig");
+const jokes = require("./jokes/jokes-data");
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+test("sanity", () => {
+  expect(true).toBe(false);
+});
